eef73dfe171da1edc75930c2c576f5a01894d2c3
AsahelF <s651032@student.roundrockisd.org>
2014-12-03 13:17:02 -0600

    70-100

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,6 +14,9 @@ root = Tk()
 
 drawpad = Canvas(root, width=800,height=600, background='white')
 player = drawpad.create_oval(390,580,410,600, fill="red")
+enemy1= drawpad.create_rectangle(50,100,70,120, fill="green")
+enemy2= drawpad.create_rectangle(390,250,410,270, fill="green")
+enemy3= drawpad.create_rectangle(780,400,800,420, fill="green")
 
 # Create your "enemies" here, before the class
 
@@ -24,30 +27,87 @@ class MyApp:
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
+       	    
        	    self.up = Button(self.myContainer1)
        	    self.up.configure(text="up", background= "green")
        	    self.up.grid(row=0,column=0)
-       	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
        	    
-       	    # No need to edit this - just includes the drawpad into our frame
+       	    
        	    drawpad.pack(side=RIGHT)
-       	    # call the animate function to start our recursion
+       	    
+       	    self.right = Button(self.myContainer1)
+       	    self.right.configure(text="right", background= "blue")
+       	    self.right.grid(row=1,column=2)
+       	    self.right.bind("<Button-1>", self.upClicked)
+       	    
+       	    self.left = Button(self.myContainer1)
+       	    self.left.configure(text="left", background= "blue")
+       	    self.left.grid(row=1,column=0)
+       	    self.left.bind("<Button-1>", self.upClicked)
+       	    
+       	    self.down = Button(self.myContainer1)
+       	    self.down.configure(text="down", background= "green")
+       	    self.down.grid(row=2,column=1)
+       	    self.down.bind("<Button-1>", self.upClicked)
        	    self.animate()
 	
-	def animate(self):
-	    global drawpad
-	    global player
-	    # Remember to include your "enemies" with "global"
-	    
-	    # Uncomment this when you're ready to test out your animation!
-	    #drawpad.after(10,self.animate)
-		
 	def upClicked(self, event):   
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
+	
+	def rightClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,20,0)
+	
+	def leftClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,-20, 0)
+	
+	def downClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,0,20)
+	
+	def animate(self):
+	    global drawpad
+	    global player
+	    global enemy1
+	    global enemy2
+	    global enemy3
+	    global direction
+	    global direction2
+	    global direction3
+	    
+	    x1,y1,x2,y2 = drawpad.coords(enemy1)
+	    if x2 > drawpad.winfo_width():
+	        direction = -800
+	    elif x1 < 0:
+	        direction = 5
+	        
+	    x1,y1,x2,y2 = drawpad.coords(enemy2)
+	    if x2 > drawpad.winfo_width():
+	        direction = -800
+	    elif x1 < 0:
+	        direction = 7
+	        
+	    x1,y1,x2,y2 = drawpad.coords(enemy2)
+	    if x2 > drawpad.winfo_width():
+	        direction = -800
+	    elif x1 < 0:
+	        direction = -9  
+	    
+	    drawpad.move(enemy1, direction, 0)
+	    drawpad.move(enemy2, direction2, 0)
+	    drawpad.move(enemy3, direction3, 0)
+	    
+	    
+	    
+	    drawpad.after(10, self,animate)
 		
-
+	
 app = MyApp(root)
 root.mainloop()
